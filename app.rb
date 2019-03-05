2948f9b7e70853677c69ed699d000a779f219228
Juan Rivera <juanriv45@gmail.com>
2019-03-05 02:42:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,16 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do
-  "Hello, World!"
+  get '/' do 
+    "Hello, World!"
   end
   get '/name' do
-  "My name is Juan"
+    "My name is Juan"
   end
   get '/hometown' do
-  "My hometown is Dallas"
+    "My hometown is Dallas"
   end
   get '/favorite-song' do
-  "My favorite song is Mr.Rager"
+    "My favorite song is Soundtrack to my life"
   end
+
+  
 end
